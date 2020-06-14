3a856e0a9b8186b5b964eb0af96a9af33fb3f1c4
kdas6141 <65910294+kdas6141@users.noreply.github.com>
2020-06-14 09:44:32 -0700

    Add files via upload

--- sort.cpp
+++ sort.cpp
@@ -1,28 +1,42 @@
 #include <iostream>
-#include <array>
+#include <vector>
+
 using namespace std;
 
-int partition(int arr[], int low, int high) {
-	int pivot = arr[high];
+int partition(vector<int> &nums, int low, int high) {
+	int pivot = nums[high];
 	int i = (low-1);
 	for (int j=low; j<=high; j++) {
-		if (arr[j] < pivot) {
+		if (nums[j] < pivot) {
 			i++;
-			swap(arr[i], arr[j]);
+			swap(nums[i], nums[j]);
 		}
 	}
-	swap(arr[i+1], arr[high]);
+	swap(nums[i+1], nums[high]);
 	return i+1;
 }
 
-void quickSort(int arr[], int low, int high) {
+void quickSort(vector<int> &nums, int low, int high) {
 	if (low < high) {
-		int pi = partition(arr, low, high);
-		quickSort(arr, low, pi-1);
-		quickSort(arr, pi+1, high);
+		int pi = partition(nums, low, high);
+		quickSort(nums, low, pi-1);
+		quickSort(nums, pi+1, high);
+	}
+}
+
+void printNums(vector<int> &nums, string header) {
+	cout << header;
+	for (auto n: nums) {
+		cout << n << " ";
 	}
 }
 
 int main() {
+	vector<int> nums = {10, 3, 7, 100, 2, 6, 8, 9, 5, 4, 3};
+
+	printNums(nums, "\nNums before sort: ");
+	quickSort(nums, 0, nums.size()-1);
+	printNums(nums, "\nNums after sort: ");
+
 	return 0;
 }
